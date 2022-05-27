eb6383f0d8d0001abf47819195cd349027d877e9
RomanZorkin <zorkin.roman@gmail.com>
2022-05-27 18:26:09 +0700

    read all boxes

--- service/imgreader/reader.py
+++ service/imgreader/reader.py
@@ -12,14 +12,17 @@ markup = Path('service/markup/11.json')
 new_path = Path('service/images/_11.jpg')
 
 
-def draw_box(image_path: Path, box: list[int]):
-    print(box)
+def draw_box(image_path: Path, boxes: list[int]):
+
     image = cv2.imread(str(image_path))
-    center_x, center_y, height, width, const = box
-    left_bottom = (int(center_x), int(center_y))
-    right_bottom = (int(center_x + height), int(center_y + width))
-    print(f'left: {left_bottom}\nright: {right_bottom}')
-    cv2.rectangle(image, left_bottom, right_bottom, (0, 255, 0), 3)
+    for in_box in boxes:
+        box = in_box['bbox']
+        print(box)
+        center_x, center_y, height, width, const = box
+        left_bottom = (int(center_x), int(center_y))
+        right_bottom = (int(center_x + height), int(center_y + width))
+        print(f'left: {left_bottom}\nright: {right_bottom}')
+        cv2.rectangle(image, left_bottom, right_bottom, (0, 255, 0), 3)
     cv2.imwrite(str(new_path), image)
 
 
@@ -30,7 +33,7 @@ def read_boxes(markup: Path) -> list[dict[str, Any]]:
 
 def run():
     marks = read_boxes(markup)
-    box = marks[0]['bbox']
+    box = marks
     draw_box(image_path, box)
 
 run()
