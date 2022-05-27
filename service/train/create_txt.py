b70a5445ae2e884fd6e8fb16cce6ec32d39c11ec
RomanZorkin <zorkin.roman@gmail.com>
2022-05-27 20:12:57 +0700

    create marks.txt

--- service/train/create_txt.py
+++ service/train/create_txt.py
@@ -3,6 +3,7 @@ import json
 from pathlib import Path
 from typing import Any
 
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
@@ -16,20 +17,33 @@ def get_json(marks_path: Path) -> list[Path]:
     return [mark_file for mark_file in marks_path.iterdir()]
 
 
-def prepare_data(mark: Path):
-    logger.debug(mark)
+def prepare_data(marks: list[dict[str, Any]], size: tuple[int, int], name: Path):
+    marks_list = []
+    for mark in marks:
+        logger.debug(f'{mark}\n')
+        if not mark.get('bbox', False):
+            continue        
+        row = ', '.join(list(map(str, mark['bbox'][:4])))
+        row = f'1 {row}\n'
+        marks_list.append(row)
+    logger.debug(marks_list)
+    create_file(name, marks_list)
 
 
-def create_file(name: Path, box: str) -> None:
-    with open(name, 'w') as label:
-        label.write(box)
+def create_file(name: Path, rows: list[str]) -> None:
+    new_name = Path(target_marks, f'{name}.txt')
+    with open(new_name, 'w') as label:
+        label.writelines(rows)
 
 
 def run():
-    marks = get_json(base_marks)
-    for mark in marks:
-        
-        prepare_data(mark)
+    json_list = get_json(base_marks)
+    for json_file in json_list:
+        with open(json_file, 'r') as j_file:
+            marks = json.loads(j_file.read())
+            size = (1, 1)
+            name = json_file.stem
+            prepare_data(marks, size, name)
 
 
 run()
