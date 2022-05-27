2f6209d613e7705d9833675e3bc44a178f22aeed
RomanZorkin <zorkin.roman@gmail.com>
2022-05-27 19:37:35 +0700

    update repo

--- service/train/create_txt.py
+++ service/train/create_txt.py
@@ -1,5 +1,35 @@
+import logging
+import json
 from pathlib import Path
+from typing import Any
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 base_marks = Path('service/markup')
 base_images = Path('service/images')
 target_marks = Path('service/train/data/warlus/labels')
+
+
+def get_json(marks_path: Path) -> list[Path]:
+    return [mark_file for mark_file in marks_path.iterdir()]
+
+
+def prepare_data(mark: Path):
+    logger.debug(mark)
+
+
+def create_file(name: Path, box: str) -> None:
+    with open(name, 'w') as label:
+        label.write(box)
+
+
+def run():
+    marks = get_json(base_marks)
+    for mark in marks:
+        
+        prepare_data(mark)
+
+
+run()
